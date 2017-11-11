fa5150c46c9ff3a3d6947ed25daa5e226e496287
hongwen000 <hongwen000@outlook.com>
2017-11-11 10:37:06 +0800

    ALU 完成代码

--- WithoutBlockDesign102022.srcs/sources_1/imports/new/ALU.v
+++ WithoutBlockDesign102022.srcs/sources_1/imports/new/ALU.v
@@ -29,4 +29,31 @@ module ALU(
     output ALU_Output_Sign
     );
     assign ALU_Output_Zero = ALU_Input_1 == ALU_Input_2 ? 1 : 0;
+    assign ALU_Output_Sign = ALU_Output_Result[31] ? 1 : 0;
+    always @(ALU_Control_in or ALU_Input_1 or ALU_Input_2)
+    begin
+        case(ALU_Control_in)
+            4'b0000: ALU_Output_Result = ALU_Input_1 & ALU_Input_2;
+            4'b0001: ALU_Output_Result = ALU_Input_1 | ALU_Input_2;
+            4'b0010: ALU_Output_Result = ALU_Input_1 + ALU_Input_2;
+            4'b0110: ALU_Output_Result = ALU_Input_1 - ALU_Input_2;
+            4'b0111: ALU_Output_Result = ALU_Input_1 < ALU_Input_2 ? 1 : 0;
+            4'b1000: 
+            begin
+                if(ALU_Input_1[31] == 1 && ALU_Input_2[31] == 0)
+                    ALU_Output_Result = 1;
+                else if ((ALU_Input_1 < ALU_Input_2) &&(ALU_Input_1[31] == ALU_Input_2[31]))
+                    ALU_Output_Result = 1;
+                else
+                    ALU_Output_Result = 0;
+            end
+            4'b1001: ALU_Output_Result = ALU_Input_2 << ALU_Input_1;
+            4'b1100: ALU_Output_Result = ALU_Input_1 ^ ALU_Input_2;
+            default:
+            begin
+                ALU_Output_Result = 32'h00000000;
+                $display("EXCEPTION@[ALU]:no match ALU Control Input")
+            end
+        endcase
+    end
 endmodule
