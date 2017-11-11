5bcdb85534c83bca8fe303d162067482e07b5264
hongwen000 <hongwen000@outlook.com>
2017-11-11 12:17:56 +0800

    完成两个Memory

--- WithoutBlockDesign102022.srcs/sources_1/imports/new/InstrMem.v
+++ WithoutBlockDesign102022.srcs/sources_1/imports/new/InstrMem.v
@@ -24,6 +24,17 @@ module InstrMem(
     input [31:0] ReadAddr,
     //output [31:0] Instrction,
     output [31:0] Instr
-
     );
+    reg[7:0] rom[127:0];
+    initial 
+    begin
+        $readmemb("rom.txt", rom);
+    end
+    always @(ReadAddr)
+    begin
+        Instr[31:24] = rom[ReadAddr];
+        Instr[23:16] = rom[ReadAddr + 1];
+        Instr[15:8]  = rom[ReadAddr + 2];
+        Instr[7:0]   = rom[ReadAddr + 3];
+    end
 endmodule
