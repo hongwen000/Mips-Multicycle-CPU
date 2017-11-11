44d1a54169b20fc67fdf03b5d6497a83cf3006c3
hongwen000 <hongwen000@outlook.com>
2017-11-11 11:40:31 +0800

    完成Control

--- WithoutBlockDesign102022.srcs/sources_1/imports/new/Control.v
+++ WithoutBlockDesign102022.srcs/sources_1/imports/new/Control.v
@@ -22,6 +22,7 @@
 
 module Control(
     input [31:26] Instr,
+    input [10:6] Funct,
     output RegDst,
     output Jump,
     output [3:0]Branch,
@@ -34,4 +35,30 @@ module Control(
     output PCWrite,
     output ZeroExt
     );
+    always @(Instr or Funct)
+    begin
+        case (Instr)
+            6'b000000:
+            begin
+                case(Funct)
+                    6'b000000: {RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b1_0_0_1_0_0_0000_0010_0_0_1_0;
+                    default: {RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b1_0_0_1_0_0_0000_0010_0_0_1_0;
+                endcase
+            end
+            4'b000100:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_0_0_0_0_0_0001_0001_0_0_1_0;
+            4'b000101:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_0_0_0_0_0_0010_0001_0_0_1_0;
+            4'b000111:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_0_0_0_0_0_0011_0011_0_0_1_0;
+            4'b001000:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_1_0_1_0_0_0000_0100_0_0_1_0;
+            4'b001101:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_1_0_1_0_0_0000_0101_0_0_1_1;
+            4'b100011:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_1_1_1_1_0_0000_0000_0_0_1_0;
+            4'b101011:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_1_0_0_0_1_0000_0000_0_0_1_0;
+            4'b000010:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_0_0_0_0_0_0000_0000_1_0_1_0;
+            4'b111111:{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_0_0_0_0_0_0000_0000_0_0_0_0;
+            default:
+            begin
+                {RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Jump,Clear,PCWrite,ZeroExt} = 18'b0_0_0_0_0_0_0000_0000_0_0_0_0;
+                $display("EXCEPTION@[Control]:no match OPCode Input");
+            end
+        endcase
+    end
 endmodule
