a0e722163832939eb880932b90ef27d4c89f0252
hongwen000 <hongwen000@outlook.com>
2017-11-11 12:03:03 +0800

    完成寄存器文件

--- WithoutBlockDesign102022.srcs/sources_1/imports/new/RegFile.v
+++ WithoutBlockDesign102022.srcs/sources_1/imports/new/RegFile.v
@@ -22,6 +22,7 @@
 
 module RegFile(
     input CLK,
+    input Clear,
     input [4:0] Read_Reg_1,
     input [4:0] Read_Reg_2,
     input [4:0] Write_Reg,
@@ -30,4 +31,17 @@ module RegFile(
     output [31:0] Read_Data_1,
     output [31:0] Read_Data_2
     );
+    reg[31:0] impl_reg[1:31];
+    assign Read_Data_1 = (Read_Reg_1 == 0) ? 0 : impl_reg[Read_Reg_1];
+    assign Read_Data_2 = (Read_Reg_2 == 0) ? 0 : impl_reg[Read_Reg_2];
+    always @(negedge CLK or posedge Clear)
+    begin
+        if(Clear) 
+        begin
+            integer i;
+            for(i = 1; i < 32; i = i + 1) impl_reg[i] <= 0;
+        end
+        else if(RegWrite_in == 1 && Write_Reg != 0)
+            impl_reg[Write_Reg] <= Write_Data;
+    end
 endmodule
